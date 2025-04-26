3a07e2ec82fc282a2937d4b449f166b392a0ab63
KarelinIvan <ivan.karelin.1993@mail.ru>
2025-04-26 18:21:41 +0300

    4-й коммит,

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
+from menu.models import MenuItem
+
+@admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'menu_name', 'parent', 'order')
     list_filter = ('menu_name',)
     search_fields = ('name', 'menu_name')
     fields = ('name', 'menu_name', 'parent', 'order', 'named_url', 'explicit_url')
+
+
